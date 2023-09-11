46b9e15409520ccaa8a89e147d0b10b3a5c06b1c
Samuel_Ramos <progsamu@gmail.com>
2023-09-11 17:04:24 -0300

    catalogação de filmes

---
+++ assets/catalog.js
@@ -0,0 +1,133 @@
+const content = [
+  
+    {
+        "title": "Captain America: The First Avenger",
+        "link": "https://youtu.be/JerVrbLldXw?si=vFqsMLKgJhuXwOt1",
+        "image": "",
+        "category": "movie",
+        "studio": "Marvel",
+        "description": "Steve Rogers is a young man who participates in experiments aimed at creating the American super soldier. When military officers manage to transform him into a human weapon, they realize that they cannot risk the young man's life in war battles."
+      },
+      {
+        "title": "Star Wars: The Rise of Skywalker",
+        "link": "https://youtu.be/8Qn_spdM5Zg?si=5bUylPkp0WHIQDFT",
+        "image": "",
+        "category": "movie",
+        "studio": "Star Wars",
+        "description": "With the return of Emperor Palpatine, the Resistance takes the lead in battle. Training to be a full-fledged Jedi, Rey finds herself conflicted with her past and future, and fears for the answers she might get from Kylo Ren."
+      },
+      {
+        "title": "SPIDER-MAN: HOMECOMING",
+        "link": "https://youtu.be/rk-dF1lIbIg?si=lqEIXKeIDtM1yp1-",
+        "image": "",
+        "category": "movie",
+        "studio": "Marvel",
+        "description": "After fighting alongside the Avengers, it's time for young Peter Parker to return home to New York alongside his Aunt May. Under the watchful eye of mentor Tony Stark, Peter begins to embrace his new identity as Spider-Man and fight daily small crimes in the neighborhood. He believes he has found his life's mission when the terrible villain Vulture appears threatening the city. The problem is that the task will not be as easy as he imagined."
+      },
+      {
+        "title": "Star Wars: The Last Jedi",
+        "link": "https://youtu.be/Q0CbN8sfihY?si=ppZX5XNinCzmNPoi",
+        "image": "",
+        "category": "movie",
+        "studio": "Star Wars",
+        "description": "Luke Skywalker's quiet and lonely life takes a turn for the worse when he meets Rey, a young woman who shows strong signs of the Force. Her desire to learn the ways of the Jedi forces Luke to make a decision that will change his life forever. Meanwhile, Kylo Ren and General Hux lead the First Order in an all-out assault against Leia and the Resistance for supremacy of the galaxy."
+      },
+      {
+        "title": "UNCHARTED",
+        "link": "https://youtu.be/eHp3MbsCbMg?si=X7woZOB8gl59IyYF",
+        "image": "",
+        "category": "movie",
+        "studio": "Sony",
+        "description": "Nathan Drake and his scoundrel partner Victor 'Sully' Sullivan embark on a dangerous quest to find the greatest treasure ever found. Meanwhile, they also track down clues that could lead to Nathan's lost brother."
+      },
+      {
+        "title": "Avengers Endgame",
+        "link": "https://youtu.be/TcMBFSGVi1c?si=A4mUTiknRTWvZVVI",
+        "image": "",
+        "category": "movie",
+        "studio": "Marvel",
+        "description": "After Thanos eliminates half of the living creatures, the Avengers must deal with the loss of friends and loved ones. With Tony Stark wandering lost in space without water and food, Steve Rogers and Natasha Romanov lead the resistance against the mad titan."
+      },
+
+    {
+        "title": "The Exorcist (1973)",
+        "link": "https://youtu.be/YDGw1MTEe9k?si=eKADqDAT_27et5Nx",
+        "image": "",
+        "category": "movie",
+        "studio": "Warner Bros.",
+        "description": "An actress gradually becomes aware that her twelve-year-old daughter is behaving completely frighteningly. Therefore, she asks for help from a priest, who is also a psychiatrist, and he comes to the conclusion that the girl is possessed by the devil. He then requests the help of a second priest, a specialist in exorcism, to try to free the girl from this terrible possession."
+    },
+    {
+        "title": "The Shining (1980)",
+        "link": "https://youtu.be/S014oGZiSdI?si=ITxlUrf_NYIvXild",
+        "image": "",
+        "category": "movie",
+        "studio": "Warner Bros.",
+        "description": "Jack Torrance becomes winter housekeeper at the isolated Overlook Hotel in the mountains of Colorado, hoping to cure his writer's block. He settles in with his wife Wendy and son Danny, who is tormented by premonitions. Jack can't write and Danny's visions become more disturbing. The writer discovers the hotel's dark secrets and begins to transform into a homicidal maniac, terrorizing his family."
+    },
+    {
+        "title": "Psycho (1960)",
+        "link": "https://youtu.be/Wz719b9QUqY?si=NBjkECYxR4hhCwXJ",
+        "image": "",
+        "category": "movie",
+        "studio": "Universal Pictures",
+        "description": "After stealing 40 thousand dollars to marry her boyfriend, a woman runs away during a storm and decides to spend the night in a hotel she finds along the way. She meets the polite and nervous owner of the establishment, Norman Bates, a young man with an interest in taxidermy and a relationship urbanized with his mother. What seems like a simple stay at the place turns into a real night of terror."
+    },
+    {
+        "title": "The Texas Chain Saw Massacre",
+        "link": "https://youtu.be/BKn9QIaMgtQ?si=6NSklm_vxx-nD6FH",
+        "image": "",
+        "category": "movie",
+        "studio": "Vortex",
+        "description": "On the way to visit their grandfather's grave, five young people get lost and are plunged into an endless nightmare when they meet a family of cannibals. Psychopaths attack outsiders using a variety of brutal and sadistic methods."
+    },
+    {
+        "title": "Halloween (1978)",
+        "link": "https://youtu.be/T5ke9IPTIJQ?si=KUxuDYsV6HBdDIOo",
+        "image": "",
+        "category": "movie",
+        "studio": "Compass International Pictures",
+        "description": "In 1963, during a cold Halloween night, six-year-old Michael Myers brutally murders his teenage sister, Judith. He is convicted and detained for 15 years in an institution under constant surveillance by Dr. Sam Loomis. In 1978, on Halloween Eve, Michael steals a car and escapes from Smith's Grove Sanitarium. He returns to his quiet hometown of Haddonfield, Illinois, where he searches for his next victims."
+    },
+    {
+        "title": "The Witch",
+        "link": "https://youtu.be/iQXmlf3Sefg?si=UWZWqa0esRreVmbl",
+        "image": "",
+        "category": "movie",
+        "studio": "A24",
+        "description": "On a farm in the 17th century, religious hysteria takes over a family that blames the eldest daughter for the disappearance of her baby brother."
+    },
+    {
+        "title": "Get Out",
+        "link": "https://youtu.be/DzfpyUB60YY?si=4uUePjh6XC6yXuwC",
+        "image": "",
+        "category": "movie",
+        "studio": "Universal Pictures",
+        "description": "Chris is a young black photographer who is about to meet the parents of Rose, his Caucasian girlfriend. At her parents' luxurious estate, Chris realizes that the family is hiding something very disturbing."
+    },
+    {
+        "title": "The Babadook",
+        "link": "https://youtu.be/k5WQZzDRVtw?si=3urIccgY69Ug53Pe",
+        "image": "",
+        "category": "movie",
+        "studio": "Causeway Films",
+        "description": "Amelia, a single mother tormented by her husband's violent death, tries to deal with her son's irrational fear that there is a monster lurking in the house, but soon discovers a sinister presence all around her."
+    },
+    {
+        "title": "Hereditary",
+        "link": "https://youtu.be/V6wWKNij_1M?si=f1X8gx02iH_fwtxu",
+        "image": "",
+        "category": "movie",
+        "studio": "A24",
+        "description": "After the death of their reclusive grandmother, the Graham family begins to unravel some things. Even after the matriarch's departure, she remains a shadow over the family, especially her lonely teenage granddaughter, Charlie, for whom she has always maintained an unusual fascination. With a growing terror gripping the house, the family explores darker places to escape the unfortunate fate they have inherited."
+    },
+    {
+        "title": "The Conjuring",
+        "link": "https://youtu.be/k10ETZ41q5o?si=jzjessoT81T066uB",
+        "image": "",
+        "category": "movie",
+        "studio": "New Line Cinema / Warner Bros.",
+        "description": "Paranormal investigators Ed and Lorraine Warren work to help a family terrorized by a demonic entity on their farm."
+    },
+
+]
