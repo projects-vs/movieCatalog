755fb8528e58304d4522ac4f81568a53f4cd32d1
ProgSamuel <progsamu@gmail.com>
2023-09-25 13:55:19 -0300

    add & bug identified

--- assets/catalog.js
+++ assets/catalog.js
@@ -1,10 +1,10 @@
-const content = [
-  
+ const content = [
     {
         "title": "Captain America: The First Avenger",
-        "link": "https://youtu.be/JerVrbLldXw?si=vFqsMLKgJhuXwOt1",
-        "image": "",
+        "link": "https://youtu.be/RZUZLqDFgkg?si=NjDMqklLtalDguT0",
+        "image": "https://i.ytimg.com/vi/RZUZLqDFgkg/maxresdefault.jpg", // get corresponding image
         "category": "movie",
+        'principal': true,
         "studio": "Marvel",
         "description": "Steve Rogers is a young man who participates in experiments aimed at creating the American super soldier. When military officers manage to transform him into a human weapon, they realize that they cannot risk the young man's life in war battles."
       },
@@ -13,6 +13,7 @@ const content = [
         "link": "https://youtu.be/8Qn_spdM5Zg?si=5bUylPkp0WHIQDFT",
         "image": "https://i.ytimg.com/vi/8Qn_spdM5Zg/maxresdefault.jpg",
         "category": "movie",
+        'principal': true,
         "studio": "Star Wars",
         "description": "With the return of Emperor Palpatine, the Resistance takes the lead in battle. Training to be a full-fledged Jedi, Rey finds herself conflicted with her past and future, and fears for the answers she might get from Kylo Ren."
       },
@@ -21,6 +22,7 @@ const content = [
         "link": "https://youtu.be/rk-dF1lIbIg?si=lqEIXKeIDtM1yp1-",
         "image": "https://i.ytimg.com/vi/rk-dF1lIbIg/maxresdefault.jpg",
         "category": "movie",
+        'principal': true,
         "studio": "Marvel",
         "description": "After fighting alongside the Avengers, it's time for young Peter Parker to return home to New York alongside his Aunt May. Under the watchful eye of mentor Tony Stark, Peter begins to embrace his new identity as Spider-Man and fight daily small crimes in the neighborhood. He believes he has found his life's mission when the terrible villain Vulture appears threatening the city. The problem is that the task will not be as easy as he imagined."
       },
@@ -29,6 +31,7 @@ const content = [
         "link": "https://youtu.be/Q0CbN8sfihY?si=ppZX5XNinCzmNPoi",
         "image": "https://i.ytimg.com/vi/Q0CbN8sfihY/maxresdefault.jpg",
         "category": "movie",
+        'principal': true,
         "studio": "Star Wars",
         "description": "Luke Skywalker's quiet and lonely life takes a turn for the worse when he meets Rey, a young woman who shows strong signs of the Force. Her desire to learn the ways of the Jedi forces Luke to make a decision that will change his life forever. Meanwhile, Kylo Ren and General Hux lead the First Order in an all-out assault against Leia and the Resistance for supremacy of the galaxy."
       },
@@ -37,6 +40,7 @@ const content = [
         "link": "https://youtu.be/eHp3MbsCbMg?si=X7woZOB8gl59IyYF",
         "image": "https://i.ytimg.com/vi/eHp3MbsCbMg/maxresdefault.jpg",
         "category": "movie",
+        'principal': true,
         "studio": "Sony",
         "description": "Nathan Drake and his scoundrel partner Victor 'Sully' Sullivan embark on a dangerous quest to find the greatest treasure ever found. Meanwhile, they also track down clues that could lead to Nathan's lost brother."
       },
@@ -45,6 +49,7 @@ const content = [
         "link": "https://youtu.be/TcMBFSGVi1c?si=A4mUTiknRTWvZVVI",
         "image": "https://i.ytimg.com/vi/TcMBFSGVi1c/maxresdefault.jpg",
         "category": "movie",
+        'principal': true,
         "studio": "Marvel",
         "description": "After Thanos eliminates half of the living creatures, the Avengers must deal with the loss of friends and loved ones. With Tony Stark wandering lost in space without water and food, Steve Rogers and Natasha Romanov lead the resistance against the mad titan."
       },
@@ -54,6 +59,7 @@ const content = [
         "link": "https://youtu.be/YDGw1MTEe9k?si=eKADqDAT_27et5Nx",
         "image": "https://i.ytimg.com/vi/YDGw1MTEe9k/maxresdefault.jpg",
         "category": "movie",
+        'principal': true,
         "studio": "Warner Bros.",
         "description": "An actress gradually becomes aware that her twelve-year-old daughter is behaving completely frighteningly. Therefore, she asks for help from a priest, who is also a psychiatrist, and he comes to the conclusion that the girl is possessed by the devil. He then requests the help of a second priest, a specialist in exorcism, to try to free the girl from this terrible possession."
     },
@@ -62,6 +68,7 @@ const content = [
         "link": "https://youtu.be/S014oGZiSdI?si=ITxlUrf_NYIvXild",
         "image": "https://i.ytimg.com/vi/S014oGZiSdI/maxresdefault.jpg",
         "category": "movie",
+        'principal': true,
         "studio": "Warner Bros.",
         "description": "Jack Torrance becomes winter housekeeper at the isolated Overlook Hotel in the mountains of Colorado, hoping to cure his writer's block. He settles in with his wife Wendy and son Danny, who is tormented by premonitions. Jack can't write and Danny's visions become more disturbing. The writer discovers the hotel's dark secrets and begins to transform into a homicidal maniac, terrorizing his family."
     },
@@ -70,6 +77,7 @@ const content = [
         "link": "https://youtu.be/Wz719b9QUqY?si=NBjkECYxR4hhCwXJ",
         "image": "https://i.ytimg.com/vi/Wz719b9QUqY/maxresdefault.jpg",
         "category": "movie",
+        'principal': false,
         "studio": "Universal Pictures",
         "description": "After stealing 40 thousand dollars to marry her boyfriend, a woman runs away during a storm and decides to spend the night in a hotel she finds along the way. She meets the polite and nervous owner of the establishment, Norman Bates, a young man with an interest in taxidermy and a relationship urbanized with his mother. What seems like a simple stay at the place turns into a real night of terror."
     },
@@ -78,6 +86,7 @@ const content = [
         "link": "https://youtu.be/BKn9QIaMgtQ?si=6NSklm_vxx-nD6FH",
         "image": "https://i.ytimg.com/vi/BKn9QIaMgtQ/maxresdefault.jpg",
         "category": "movie",
+        'principal': false,
         "studio": "Vortex",
         "description": "On the way to visit their grandfather's grave, five young people get lost and are plunged into an endless nightmare when they meet a family of cannibals. Psychopaths attack outsiders using a variety of brutal and sadistic methods."
     },
@@ -86,6 +95,7 @@ const content = [
         "link": "https://youtu.be/T5ke9IPTIJQ?si=KUxuDYsV6HBdDIOo",
         "image": "https://i.ytimg.com/vi/T5ke9IPTIJQ/maxresdefault.jpg",
         "category": "movie",
+        'principal': false,
         "studio": "Compass International Pictures",
         "description": "In 1963, during a cold Halloween night, six-year-old Michael Myers brutally murders his teenage sister, Judith. He is convicted and detained for 15 years in an institution under constant surveillance by Dr. Sam Loomis. In 1978, on Halloween Eve, Michael steals a car and escapes from Smith's Grove Sanitarium. He returns to his quiet hometown of Haddonfield, Illinois, where he searches for his next victims."
     },
@@ -94,6 +104,7 @@ const content = [
         "link": "https://youtu.be/iQXmlf3Sefg?si=UWZWqa0esRreVmbl",
         "image": "https://i.ytimg.com/vi/iQXmlf3Sefg/maxresdefault.jpg",
         "category": "movie",
+        'principal': false,
         "studio": "A24",
         "description": "On a farm in the 17th century, religious hysteria takes over a family that blames the eldest daughter for the disappearance of her baby brother."
     },
@@ -102,6 +113,7 @@ const content = [
         "link": "https://youtu.be/DzfpyUB60YY?si=4uUePjh6XC6yXuwC",
         "image": "https://i.ytimg.com/vi/DzfpyUB60YY/maxresdefault.jpg",
         "category": "movie",
+        'principal': false,
         "studio": "Universal Pictures",
         "description": "Chris is a young black photographer who is about to meet the parents of Rose, his Caucasian girlfriend. At her parents' luxurious estate, Chris realizes that the family is hiding something very disturbing."
     },
@@ -110,6 +122,7 @@ const content = [
         "link": "https://youtu.be/k5WQZzDRVtw?si=3urIccgY69Ug53Pe",
         "image": "https://i.ytimg.com/vi/k5WQZzDRVtw/maxresdefault.jpg",
         "category": "movie",
+        'principal': false,
         "studio": "Causeway Films",
         "description": "Amelia, a single mother tormented by her husband's violent death, tries to deal with her son's irrational fear that there is a monster lurking in the house, but soon discovers a sinister presence all around her."
     },
@@ -118,6 +131,7 @@ const content = [
         "link": "https://youtu.be/V6wWKNij_1M?si=f1X8gx02iH_fwtxu",
         "image": "https://i.ytimg.com/vi/V6wWKNij_1M/maxresdefault.jpg",
         "category": "movie",
+        'principal': false,
         "studio": "A24",
         "description": "After the death of their reclusive grandmother, the Graham family begins to unravel some things. Even after the matriarch's departure, she remains a shadow over the family, especially her lonely teenage granddaughter, Charlie, for whom she has always maintained an unusual fascination. With a growing terror gripping the house, the family explores darker places to escape the unfortunate fate they have inherited."
     },
@@ -126,8 +140,11 @@ const content = [
         "link": "https://youtu.be/k10ETZ41q5o?si=jzjessoT81T066uB",
         "image": "https://i.ytimg.com/vi/k10ETZ41q5o/maxresdefault.jpg",
         "category": "movie",
+        'principal': false,
         "studio": "New Line Cinema / Warner Bros.",
         "description": "Paranormal investigators Ed and Lorraine Warren work to help a family terrorized by a demonic entity on their farm."
     },
 
 ]
+
+console.log(content);
--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,11 @@
 const rateStar = document.querySelector(".rateStar");
 const cardMovie = document.querySelectorAll(".cardMovie");
 
+// bug detected
+
+// 1 - card without cover
+// 2 - card does not receive click
+
 function eventManager() {
   rateStar.addEventListener("click", () => {
     rateStar.classList.remove("fa-regular");
@@ -9,11 +14,28 @@ function eventManager() {
 
   cardMovie.forEach((card) => {
     card.addEventListener("click", () => {
+      console.log("Card clicado");
       cardMovie.forEach((c) => {
         c.style.transform = "scale(1)";
       });
-      card.style.transform = "scale(1.2)";
+      card.style.transform = "scale(1.3)";
     });
   });
 }
+
+const sectionPopularMovies = document.getElementById("sectionPopularMovies");
+function generateMainSession(){
+  for (const i of content) {
+    i.principal? (sectionPopularMovies.innerHTML += `
+    <div class="col-md-2 p-3 ">
+        <div class="card cardMovie bg-transparent border border-0">
+            <img src="${i.image}" class="card-img" alt="${i.title}"/>
+        </div>
+     </div>
+    `):null
+  }
+}
+
+generateMainSession();
 eventManager();
+
