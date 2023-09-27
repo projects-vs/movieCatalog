d26e97b444d3f86ff95e3fdb27ff2b119b882403
ProgSamuel <progsamu@gmail.com>
2023-09-27 15:41:00 -0300

    add - caroussel

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,56 @@
 function eventManager() {
-  const rateStar = document.querySelector(".rateStar");
-  const cardMovies = document.querySelectorAll(".cardMovie");
+  const rateStar = document.querySelector('.rateStar');
+  const cardMovies = document.querySelectorAll('.cardMovie');
 
-  rateStar.addEventListener("click", () => {
-    rateStar.classList.remove("fa-regular");
-    rateStar.classList.add("fa-solid");
+  rateStar.addEventListener('click', () => {
+    rateStar.classList.remove('fa-regular');
+    rateStar.classList.add('fa-solid');
   });
 
   cardMovies.forEach((card) => {
-    card.addEventListener("click", () => {
-      console.log("Card clicado");
+    card.addEventListener('click', () => {
+      console.log('Card clicado');
       cardMovies.forEach((c) => {
-        c.style.transform = "scale(1)";
+        c.style.transform = 'scale(1)';
       });
-      card.style.transform = "scale(1.2)";
+      card.style.transform = 'scale(1.2)';
     });
   });
 }
 
-const sectionPopularMovies = document.getElementById("sectionPopularMovies");
-function generateMainSession(){
+const sectionPopularMovies = document.getElementById('sectionPopularMovies');
+function generateMainSession() {
   for (const i of content) {
-    i.principal? (sectionPopularMovies.innerHTML += `
-    <div class="col-md-2 p-3 ">
-        <div class="card cardMovie bg-transparent border border-0">
-            <img src="${i.image}" class="card-img" alt="${i.title}"/>
-        </div>
-     </div>
-    `):null
+    i.principal
+      ? (sectionPopularMovies.innerHTML += `
+    <li class="glide__slide col-md-2 p-3 ">
+      <div class="card cardMovie bg-transparent border border-0">
+       <img src="${i.image}" class="card-img" alt="${i.title}"/>
+      </div>
+    </li>
+    `)
+      : null;
   }
 }
 
 generateMainSession();
 eventManager();
+
+var glide = new Glide('.glide', {
+  type: 'carousel',
+  perView: 5,
+  autoplay: 2000,
+  focusAt: 'center',
+  breakpoints: {
+    800: {
+      perView: 2,
+    },
+    480: {
+      perView: 1,
+    },
+  },
+});
+
+
+glide.mount();
 
